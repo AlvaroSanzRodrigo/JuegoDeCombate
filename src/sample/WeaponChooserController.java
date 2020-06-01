c19cc8934a47917b14be1200fb2ea31a87180115
AlvaroSanzRodrigo <alvaro.sanz@u-tad.com>
2020-06-01 20:00:03 +0200

    Ya está

--- src/sample/WeaponChooserController.java
+++ src/sample/WeaponChooserController.java
@@ -1,8 +1,10 @@
 package sample;
 
 import javafx.collections.FXCollections;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,9 +15,30 @@ public class WeaponChooserController {
 
     @FXML
     public void initialize() {
-        // change next line to DB load
+        // Cargamos la lista de armas
         List<String> values = GameController.getInstance().getPlayer().getWeapons().stream().map(Weapon::getName).collect(Collectors.toList());
         weaponList.setItems(FXCollections.observableList(values));
+
+        weaponList.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+
+                // asignamos el arma al jugador
+                GameController.getInstance().getPlayer().setCurrentWeapon(
+                        (GameController.getInstance()
+                        .getPlayer()
+                        .getWeapons()
+                        .stream()
+                        .filter(weapon -> weaponList
+                                .getSelectionModel()
+                                .getSelectedItem()
+                                .toString().equals(weapon.getName()))
+                        .findFirst()
+                        .orElse(null)));
+                
+            }
+        });
 
     }
 
